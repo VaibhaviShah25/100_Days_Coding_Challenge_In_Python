8ade002558674a3dc410db4ea4ab8a6c3dd2df08
Vaibhavi Shah <100584845+VaibhaviShah25@users.noreply.github.com>
2024-01-28 12:14:35 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,9 @@
+#Printing Introduction
+#Day 1 of 100 Days coding challenge
+print("<>"*40)
+print("Hello Everyone!")
+print("My name is Vaibhavi Shah")
+print("I am a student at International Institute of Information Technology, Pune")
+print("I am pursuing B.E. in CS")
+print("Lastly, I like to learn Coding is Fun!")
+print("<>"*40)
